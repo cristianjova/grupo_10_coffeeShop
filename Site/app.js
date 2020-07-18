1a2bf9b01ce2f58984dd754938c217fb2a923ec8
Cristian Jovanovich <cristian.jova149@gmail.com>
2020-07-18 09:35:14 -0300

    Iniciado app con express y agregado rutas a productCart y productDetail

--- Site/app.js
+++ Site/app.js
@@ -1,1 +1,22 @@
+const express = require('express');
 
+// Iniciar app
+const app = express();
+
+// Archivos estaticos
+app.use(express.static('public'));
+
+// Rutas - Temporales
+app.get('/product-detail', (req, res) => {
+  res.sendFile(`${__dirname}/views/productDetail.html`);
+  // Eso es igual a (__dirname + '/views/productDetail.html') a mi me parece más facil asi
+  // Pero si no les gusta o no les parece usamos con el + y listo :)
+});
+
+app.get('/product-cart', (req, res) => {
+  res.sendFile(__dirname + '/views/productCart.html');
+});
+
+// Correr servidor
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
