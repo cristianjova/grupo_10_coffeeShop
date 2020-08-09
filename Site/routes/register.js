e5ceca2b5a186623e6c49c8e1c35cc4e949a2b7f
Alejandro Galeano <ale.gb32@gmail.com>
2020-08-09 15:25:24 -0300

    cambio de nombre de archivo de rutas de registro a usuarios

--- Site/routes/register.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/userController');
-
-router.get('/register', controller.register);
-// Procesamiento del formulario de creación
-router.post('/register', controller.store);
-
-module.exports = router;
