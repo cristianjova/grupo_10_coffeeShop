363b98e97386c23b6dd715d475a2f86c4e58eb13
Cristian Jovanovich <cristian.jova149@gmail.com>
2020-08-10 21:48:29 -0300

    S3T05 - productsController(create, edit)

--- Site/controllers/productsController.js
+++ Site/controllers/productsController.js
@@ -3,6 +3,12 @@ module.exports = {
   detail: (req, res) => {
     res.render('products/productDetail');
   },
+  store: (req, res) => {
+    res.render('products/create');
+  },
+  edit: (req, res) => {
+    res.render('products/edit');
+  },
   cart: (req, res) => {
     res.render('products/productCart');
   }
