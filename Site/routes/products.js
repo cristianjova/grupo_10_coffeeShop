34441826bf4c8a0b3452c142e5a07e0970125e2b
Cristian Jovanovich <cristian.jova149@gmail.com>
2020-08-10 21:48:59 -0300

    S3T11 - productsRoute(create,edit)

--- Site/routes/products.js
+++ Site/routes/products.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const controller = require('../controllers/productsController');
 
 router.get('/detail', controller.detail);
-
+router.get('/create', controller.store)
+router.get('/:id/edit', controller.edit)
 router.get('/cart', controller.cart);
 
 module.exports = router;
