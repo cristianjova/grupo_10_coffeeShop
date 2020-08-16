b379ec040ab6f28d6822035d26361e76adbcdfe3
niconiz <niznicolas97@gmail.com>
2020-08-16 19:15:57 -0300

    JSON JS

---
+++ Site/database/jsontable.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+let model = function (tableName) {
+    return {
+        
+        filePath: path.join(__dirname, '../data/' + tableName + '.json'),
+
+        readFile() {
+            let fileContents = fs.readFileSync(this.filePath, 'utf-8');
+        
+            if(fileContents) {
+                return JSON.parse(fileContents);
+            }
+        
+            return [];
+        },
+
+        find(id) {
+            let rows = this.readFile();
+            return rows.find(row => row.id == id);
+        },
+    }
+
+}
+
+module.exports(model);
