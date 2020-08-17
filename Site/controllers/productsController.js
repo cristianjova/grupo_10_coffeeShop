7e03db86b1b47323a1275fd798f548f24c59e6b9
niconiz <niznicolas97@gmail.com>
2020-08-16 22:19:50 -0300

    S4-T01 /products (GET) S4-T02 /products/:id (GET)

--- Site/controllers/productsController.js
+++ Site/controllers/productsController.js
@@ -1,7 +1,27 @@
+const fs = require('fs');
+const path = require ('path');
+const tableName = require ('../database/jsontable');
+
+const productsModel = tableName('products');
+
 module.exports = {
-  index: {},
+  index: (req,res) => {
+
+    let products = productsModel.all();
+     
+
+
+    res.render('products/list', {products});
+  },
   detail: (req, res) => {
-    res.render('products/detail');
+    let id = req.params.id;
+    let product = productsModel.find(id);
+    if (product){
+      res.render('products/detail', {product});
+    } else {
+      res.render("No disponible");
+    }
+    
   },
   store: (req, res) => {
     res.render('products/create');
