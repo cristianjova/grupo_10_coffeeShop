54a73471e153bf825b8567702774775ca33c0e52
Alejandro Galeano <ale.gb32@gmail.com>
2020-08-18 16:17:53 -0300

    se desarrolla la Funcionalidad de registro de usuario, modificacion del formulario y ruta para carga de imagenes

--- Site/database/jsontable.js
+++ Site/database/jsontable.js
@@ -5,6 +5,20 @@ let model = function (tableName) {
     return {
 
         filePath: path.join(__dirname, '../data/' + tableName +'.json'),
+        nextId() {
+            let rows = this.readFile();
+            let lastRow = rows.pop();
+
+            if (lastRow) {
+                return ++lastRow.id;
+            }
+
+            return 1;
+        },
+        writeFile(contents) {
+            let fileContents = JSON.stringify(contents, null, " ");
+            fs.writeFileSync(this.filePath, fileContents);
+        },
 
         readFile() {
             let fileContents = fs.readFileSync(this.filePath, 'utf-8');
@@ -24,6 +38,20 @@ let model = function (tableName) {
             let rows = this.readFile();
             return rows.find(row => row.id == id);
         },
+        create(row) {
+            // Microdesafío
+            // 1. Leer el archivo
+            // 2. Generar un nuevo id
+            // 3. Agregar el registro
+            // 4. guardar los cambios
+            let rows = this.readFile();
+            row.id = this.nextId();
+            rows.push(row);
+
+            this.writeFile(rows);
+
+            return row.id;
+        }
     }
 
 }
